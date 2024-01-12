5e47932cdca5df62fa68d75275a64b0b37691ca3
exploitenomah <iksploiting@gmail.com>
2024-01-12 18:01:21 +0100

    setup chakra theme

--- app/_config/chakra-theme.ts
+++ app/_config/chakra-theme.ts
@@ -2,11 +2,36 @@
 import { extendTheme } from '@chakra-ui/react'
 
 const colors = {
-  brand: {
-    900: '#1a365d',
-    800: '#153e75',
-    700: '#2a69ac',
+  white: {
+    main: "#FFFFFF",
+    100: "#F1F1F1",
+    200: "#F4F4F4",
+    300: "#F9F9F9",
+    400: "#EEEEEE",
+    500: "#E5E5E5",
+
   },
+  brand: {
+    main: '#3A86FF',
+    10: "#3A86FF1A",
+    50: "#3A86FF80",
+    100: "#5DB8EA",
+  },
+  green: {
+    main: "#009A49",
+    50: "#49C3A733",
+    100: "#49C3A7",
+  },
+  red: {
+    main: "#FE251B",
+    50: "#FF00004D"
+  },
+  gray: {
+    main: "#707070",
+    100: "#A1A1A1",
+    200: "#D9D9D9",
+    300: "#5C5F62"
+  }
 }
 
 const fonts = {
