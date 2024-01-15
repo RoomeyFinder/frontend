0dc09d68bd876ae885593896afdeecd72e5dcffb
exploitenomah <iksploiting@gmail.com>
2024-01-15 01:01:08 +0100

    refactor

--- app/_components/DropDownWithChevron.tsx
+++ app/_components/DropDownWithChevron.tsx
@@ -1,10 +1,12 @@
-import { Popover, PopoverTrigger, Flex, PopoverContent, Box, } from "@chakra-ui/react"
+import { Popover, PopoverTrigger, Flex, PopoverContent, Box, FlexProps, } from "@chakra-ui/react"
 import DownChevron from "../_assets/DownChevron"
 
 
-export default function DropDownWithChevron({ DropdownContent, DropdownTrigger }: {
+export default function DropDownWithChevron({ DropdownContent, DropdownTrigger, justifyTrigger = "center", triggerContainerProps }: {
   DropdownContent: ({ isOpen, onClose, forceUpdate }: { isOpen: boolean, onClose: () => void, forceUpdate?: () => void }) => JSX.Element
   DropdownTrigger: ({ isOpen, onClose, forceUpdate }: { isOpen: boolean, onClose: () => void, forceUpdate?: () => void }) => JSX.Element
+  justifyTrigger?: "center" | "space-between" | "space-around" | "start"
+  triggerContainerProps?: FlexProps
 }){
   return (
     <Popover placement='top-start'>
@@ -14,7 +16,7 @@ export default function DropDownWithChevron({ DropdownContent, DropdownTrigger }
             <Flex
               as="button" type="button"
               alignItems="center"
-              justifyContent="center"
+              justifyContent={justifyTrigger}
               gap="1rem"
               rounded=".8rem"
               p={{ base: "1rem", md: "0" }}
@@ -23,6 +25,7 @@ export default function DropDownWithChevron({ DropdownContent, DropdownTrigger }
               borderColor="white.200"
               width="100%"
               color={isOpen ? "black" : "gray.main"}
+              {...(triggerContainerProps || {})}
             >
               <DropdownTrigger isOpen={isOpen} onClose={onClose} forceUpdate={forceUpdate} />
               <Box transition="transform 250ms ease" transform={isOpen ? "rotateX(180deg)" : "rotate(0deg)"}>
