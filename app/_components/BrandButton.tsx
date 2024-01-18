a757dc78722915aeb9297b535f4360e59d7f897f
exploitenomah <iksploiting@gmail.com>
2024-01-18 05:15:57 +0100

    remove unused file

--- app/_components/BrandButton.tsx
+++
@@ -1,9 +0,0 @@
-
-
-import { BoxProps, ButtonProps, LinkProps, Button, useStyleConfig } from "@chakra-ui/react"
-
-export default function BrandButton(props: BoxProps & ButtonProps & LinkProps & { variant: string }) {
-  const { variant, ...rest } = props
-  const styles = useStyleConfig("BrandButton", { variant })
-  return <Button __css={styles} {...rest} />
-}
