a014b9dab8f1330009ca84d7c26f23a4cc13029e
exploitenomah <iksploiting@gmail.com>
2024-01-19 08:09:00 +0100

    create custom checkbox

---
+++ app/_components/CustomCheckbox.tsx
@@ -0,0 +1,33 @@
+"use client"
+import { Flex, Input, Text, useCheckbox, ScaleFade, chakra, TextProps } from "@chakra-ui/react"
+import { ReactNode } from "react"
+import CheckMarkFilled from "../_assets/CheckMarkFilled"
+
+
+export default function CustomCheckbox({ children, labelProps = {} }: {
+  children: ReactNode | ReactNode[]
+  labelProps?: TextProps
+}){
+  const { state, getCheckboxProps, getInputProps, getLabelProps, htmlProps } =
+    useCheckbox()
+  return (
+    <chakra.label  {...htmlProps} cursor="pointer">
+      <Input {...getInputProps()} hidden />
+      <Flex gap="1rem" alignItems="center">
+        <Flex 
+          w="2rem" 
+          h="2rem" 
+          borderRadius={state.isChecked ? "50%" : ".5rem"}
+          border="2px solid" 
+          borderColor="brand.main"
+          justifyContent="center"
+          alignItems="center" {...getCheckboxProps()}>
+          <ScaleFade initialScale={0.9} in={state.isChecked}>
+            <CheckMarkFilled />
+          </ScaleFade>
+        </Flex>
+        <Text {...labelProps} {...getLabelProps()}>{children}</Text>
+      </Flex>
+    </chakra.label>
+  )
+}
