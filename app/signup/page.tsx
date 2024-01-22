7d74665fb00dfe7bbfec78d9c2f938a9ef9d9dc2
exploitenomah <iksploiting@gmail.com>
2024-01-22 14:59:37 +0100

    refactor

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -25,21 +25,27 @@ function SignupConsumer(){
     totalStages,
     handleSubmitButtonClick,
     profileInitials,
+    contactDetails,
+    locationDetails,
     handleFormDataChange
   } = useContext(SignupContext)
 
   return (
     <Box py={{ base: "5rem", md: "" }}>
-      <AuthFormLayout handleSubmit={handleSubmitButtonClick} heading="Create Account" mode="signup" submitButtonText={totalStages.currentStage === 2 && emailVerificationAndAddressFlow.currentStage === 2 ? "Complete" : "Next"}>
+      <AuthFormLayout 
+        handleSubmit={handleSubmitButtonClick} 
+        heading="Create Account" mode="signup" 
+        submitButtonVariant={emailVerificationAndAddressFlow.currentStage === 2 ? "brand" : "brand-secondary"}
+        submitButtonText={totalStages.currentStage === 2 && emailVerificationAndAddressFlow.currentStage === 2 ? "Complete" : "Next"}>
         <Box as="form" onSubmit={(e: FormEvent) => {e.preventDefault()}}>
           <SignupProgress progressOnePercentage={profileAndContactFlow.progressInPercentage} progressTwoPercentage={emailVerificationAndAddressFlow.progressInPercentage} />
           <Stage currentStage={totalStages.currentStage} stage={1}>
             <Stage currentStage={profileAndContactFlow.currentStage} stage={1}><ProfileInitialsForm handleChange={handleFormDataChange} formData={profileInitials.formData} sectionName={profileInitials.name} /></Stage>
-            <Stage currentStage={profileAndContactFlow.currentStage} stage={2}><ContactForm /></Stage>
+            <Stage currentStage={profileAndContactFlow.currentStage} stage={2}><ContactForm handleChange={handleFormDataChange} formData={contactDetails.formData} sectionName={contactDetails.name} /></Stage>
           </Stage>
           <Stage currentStage={totalStages.currentStage} stage={2}>
             <Stage currentStage={emailVerificationAndAddressFlow.currentStage} stage={1}><EmailVerficationForm /></Stage>
-            <Stage currentStage={emailVerificationAndAddressFlow.currentStage} stage={2}><AddressForm /></Stage>
+            <Stage currentStage={emailVerificationAndAddressFlow.currentStage} stage={2}><AddressForm handleChange={handleFormDataChange} formData={locationDetails.formData} sectionName={locationDetails.name} /></Stage>
           </Stage>
         </Box>
       </AuthFormLayout>
