3115a7c509cf8add752ebb0e56c70bf8ab4934e6
Exploit <exploitenomah@gmail.com>
2024-05-17 19:01:12 +0100

    Revert "Bug fix/address input"

--- app/signup/_types.ts
+++ app/signup/_types.ts
@@ -1,7 +1,6 @@
 
 
 export type SignupValue = {
-  isSignupDone: boolean
   loading: boolean
   formErrors: string[]
   profileAndContactFlow: {
@@ -23,27 +22,10 @@ export type SignupValue = {
     progressInPercentage: number
   }
   handleSubmitButtonClick: () => void
-  profileInitials: {
-    name: string
-    formData: { [x: string]: string | number | boolean }
-    validate: (formData: {
-      [x: string]: string | boolean
-    }) => (boolean | string[])[]
-  }
-  contactDetails: {
-    name: string
-    formData: { [x: string]: string }
-    validate: (formData: { [x: string]: string }) => (boolean | string[])[]
-  }
-  emailVerificationDetails: {
-    name: string
-    formData: { [x: string]: string }
-    validate: (formData: { [x: string]: string }) => (boolean | string[])[]
-  }
-  handleFormDataChange: (
-    stageName: string,
-    name: string,
-    value: string | number | boolean
-  ) => void
+  profileInitials: { name: string, formData: { [x: string]: string | number | boolean }, validate: (formData: { [x: string]: string | boolean }) => (boolean | string[])[]}
+  contactDetails: { name: string, formData: { [x: string]: string }, validate: (formData: { [x: string]: string }) => (boolean | string[])[] }
+  emailVerificationDetails: { name: string, formData: { [x: string]: string }, validate: (formData: { [x: string]: string }) => (boolean | string[])[] }
+  locationDetails: { name: string, formData: { [x: string]: string }, validate: (formData: { [x: string]: string }) => (boolean | string[])[] }
+  handleFormDataChange: (stageName: string, name: string, value: string | number | boolean) => void
   resendVerificationEmail: () => void
 }
