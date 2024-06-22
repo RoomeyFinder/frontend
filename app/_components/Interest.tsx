af01a9c8653f57fb72cc7a9046d6b058eebcd0f6
exploitenomah <iksploiting@gmail.com>
2024-06-23 00:37:21 +0100

    update interests ui

--- app/_components/Interest.tsx
+++ app/_components/Interest.tsx
@@ -68,24 +68,27 @@ export default function InterestComponent({
       w="full"
     >
       <Avatar
-        w={{ base: "4rem", md: "7rem" }}
-        h={{ base: "4rem", md: "7rem" }}
+        w={{ base: "7rem", md: "7rem" }}
+        h={{ base: "7rem", md: "7rem" }}
         border="1px solid #3A86FF"
         src={profileToDisplay?.profileImage?.secure_url}
         name={`${profileToDisplay?.firstName} ${profileToDisplay?.lastName}`}
       />
-      <Flex flexDir="column" gap={{ base: ".5rem" }}>
+      <Flex flexDir="column" gap=".4rem">
         <Heading
           fontSize={{ base: "1.4rem", md: "1.9rem" }}
           textTransform="capitalize"
           as="h4"
           fontWeight="600"
+          onClick={() => router.push(`/users/${profileToDisplay?._id}`)}
         >
           {profileToDisplay?.firstName} {profileToDisplay?.lastName} <br />
           <Text
+            mt=".5rem"
             as="span"
+            display="block"
             fontWeight="normal"
-            fontSize="1.2rem"
+            fontSize="1.4rem"
             textTransform="lowercase"
             color="gray.main"
           >
@@ -96,112 +99,91 @@ export default function InterestComponent({
           </Text>
         </Heading>
         <Box>
-          <Text
-            as="button"
-            color="gray.main"
-            fontWeight="600"
+          <Flex
+            ml="auto"
+            gap=".8rem"
+            alignItems="center"
             fontSize={{ base: "1.2rem", md: "1.6rem" }}
-            textDecor="underline"
-            onClick={() => router.push(`/users/${profileToDisplay?._id}`)}
+            fontWeight="700"
           >
-            View profile
-          </Text>
-          &nbsp;&nbsp;&nbsp;
-          <Text
-            as="span"
-            display={{ md: "none" }}
-            fontSize={{ base: "1.3rem", md: "1.6rem" }}
-            color="#A1A1A1"
-          >
-            <TimeSince date={interest.createdAt} />
-          </Text>
-        </Box>
-      </Flex>
-      <Flex
-        ml="auto"
-        gap=".8rem"
-        alignItems="center"
-        fontSize={{ base: "1.2rem", md: "1.6rem" }}
-        fontWeight="700"
-      >
-        {isPending && (
-          <>
-            {isSent ? (
+            {isPending && (
               <>
-                <Button
-                  isLoading={loading}
-                  onClick={() =>
-                    setConfirmation({
-                      show: true,
-                      text: "Are you show you want to unsend your interest?",
-                      confirmAction: handleUnsend,
-                    })
-                  }
-                  bg="transparent"
-                  color="gray.main"
-                  aria-label="unsend interest"
-                >
-                  Unsend
-                </Button>
-              </>
-            ) : (
-              <>
-                <Text
-                  onClick={handleAccept}
-                  as="button"
-                  color="brand.main"
-                  aria-label="accept interest"
-                  title="accept interest"
-                >
-                  <Show below="md">
-                    <CheckIcon />
-                  </Show>
-                  <Show above="md">Accept</Show>
-                </Text>
-                <Text
-                  onClick={() =>
-                    setConfirmation({
-                      show: true,
-                      text: "Are you show you want to decline this interest?",
-                      confirmAction: handleDecline,
-                    })
-                  }
-                  as="button"
-                  color="gray.main"
-                  aria-label="decline interest"
-                  title="decline interest"
-                >
-                  <Show below="md">
-                    <TimesIconSmall />
-                  </Show>
-                  <Show above="md">Decline</Show>
-                </Text>
+                {isSent ? (
+                  <>
+                    <Text
+                      disabled={loading}
+                      as="button"
+                      onClick={() =>
+                        setConfirmation({
+                          show: true,
+                          text: "Are you show you want to unsend your interest?",
+                          confirmAction: handleUnsend,
+                        })
+                      }
+                      p="0"
+                      bg="transparent"
+                      color="gray.main"
+                      aria-label="unsend interest"
+                    >
+                      Unsend
+                    </Text>
+                  </>
+                ) : (
+                  <>
+                    <Text
+                      onClick={handleAccept}
+                      as="button"
+                      color="brand.main"
+                      aria-label="accept interest"
+                      title="accept interest"
+                    >
+                      Accept
+                    </Text>
+                    <Text
+                      onClick={() =>
+                        setConfirmation({
+                          show: true,
+                          text: "Are you show you want to decline this interest?",
+                          confirmAction: handleDecline,
+                        })
+                      }
+                      as="button"
+                      color="gray.main"
+                      aria-label="decline interest"
+                      title="decline interest"
+                    >
+                      Decline
+                    </Text>
+                  </>
+                )}
               </>
             )}
-          </>
-        )}
-        {!isPending && (
-          <Text
-            as="button"
-            onClick={() =>
-              interest.accepted &&
-              router.push(
-                `/messenger?otherUser=${interest.sender._id === user?._id ? interest.docOwner : interest.sender._id}`
-              )
-            }
-            color={interest.accepted ? "brand.main" : "gray.main"}
-            fontWeight="500"
-            aria-label="decline interest"
-          >
-            {interest.accepted ? "Send message" : "Declined"}
-          </Text>
-        )}
-        <Show above="md">
-          <Text fontSize={{ base: "1.3rem", md: "1.6rem" }} color="#A1A1A1">
-            <TimeSince date={interest.createdAt} />
-          </Text>
-        </Show>
+            {!isPending && (
+              <Text
+                as="button"
+                onClick={() =>
+                  interest.accepted &&
+                  router.push(
+                    `/messenger?otherUser=${interest.sender._id === user?._id ? interest.docOwner : interest.sender._id}`
+                  )
+                }
+                color={interest.accepted ? "brand.main" : "gray.main"}
+                fontWeight="500"
+                aria-label="decline interest"
+              >
+                {interest.accepted ? "Send message" : "Declined"}
+              </Text>
+            )}
+          </Flex>
+        </Box>
       </Flex>
+      <Text
+        fontSize={{ base: "1.3rem", md: "1.6rem" }}
+        ml="auto"
+        color="#A1A1A1"
+      >
+        <TimeSince date={interest.createdAt} />
+      </Text>
       <Modal isOpen={confirmation.show} isCentered onClose={hideConfirmation}>
         <ModalOverlay />
         <ModalContent
