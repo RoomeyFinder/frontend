d13618db1fe7239623ee8f70dd610893c149a48c
exploitenomah <iksploiting@gmail.com>
2024-06-23 18:57:31 +0100

    build listings search page

---
+++ app/_components/Search/SearchBar.tsx
@@ -0,0 +1,213 @@
+import { Flex, Input, Select } from "@chakra-ui/react"
+import { ChangeEventHandler, useEffect, useState } from "react"
+import toast from "react-hot-toast"
+
+export default function SearchBar({
+  handleCoordinatesChange,
+  handleBedroomChange,
+  handleRentDurationChange,
+  handleRentChange,
+}: {
+  handleCoordinatesChange: (arg: { lat: number; lng: number } | null) => void
+  handleBedroomChange: ChangeEventHandler<HTMLSelectElement>
+  handleRentDurationChange: ChangeEventHandler<HTMLSelectElement>
+  handleRentChange: ChangeEventHandler<HTMLSelectElement>
+}) {
+  useEffect(() => {
+    const center = { lat: 9.082, lng: 8.6753 }
+    // Create a bounding box with sides ~10km away from the center point
+    const defaultBounds = {
+      north: center.lat + 0.1,
+      south: center.lat - 0.1,
+      east: center.lng + 0.1,
+      west: center.lng - 0.1,
+    }
+    const input = document.getElementById("pac-input") as HTMLInputElement
+    const options = {
+      bounds: defaultBounds,
+      componentRestrictions: { country: "ng" },
+      fields: ["address_components", "geometry", "icon", "name"],
+      strictBounds: false,
+      type: "(regions)",
+    }
+    const autocomplete = new window.google.maps.places.Autocomplete(
+      input,
+      options
+    )
+    autocomplete.addListener("place_changed", () => {
+      if (autocomplete.getPlace().geometry?.location) {
+        const coordinates = {
+          lat: autocomplete.getPlace().geometry?.location?.lat(),
+          lng: autocomplete.getPlace().geometry?.location?.lng(),
+        }
+        coordinates.lat !== undefined &&
+          coordinates.lng !== undefined &&
+          handleCoordinatesChange(coordinates as any)
+      } else
+        toast.error(
+          "Unable to get the coordinates of that location. Please try another location."
+        )
+    })
+  }, [handleCoordinatesChange])
+  return (
+    <>
+      <Flex
+        w="full"
+        flexWrap="wrap"
+        justifyContent="space-between"
+        alignItems="center"
+        gap="1rem"
+      >
+        <Input
+          variant="filled"
+          py="1rem"
+          px="1rem"
+          rounded=".5rem"
+          placeholder="Find location"
+          maxW={{ md: "50rem" }}
+          id="pac-input"
+          onChange={(e) =>
+            e.target.value === "" && handleCoordinatesChange(null)
+          }
+        />
+        <Flex gap="1rem" flexGrow="1" maxW="60rem">
+          <RentDurationFilter handleSelection={handleRentDurationChange} />
+          <RentFilter handleSelection={handleRentChange} />
+          <NumberOfBedroomsFilter handleSelection={handleBedroomChange} />
+        </Flex>
+      </Flex>
+    </>
+  )
+}
+
+const NumberOfBedroomsFilter = ({
+  handleSelection,
+}: {
+  handleSelection: ChangeEventHandler
+}) => {
+  const [value, setValue] = useState("")
+  return (
+    <Input
+      rounded=".5rem"
+      fontSize="1.6rem"
+      placeholder="Bedrooms"
+      _focus={{
+        borderColor: "brand.main",
+      }}
+      value={value}
+      onChange={(e) => {
+        handleSelection(e)
+        setValue(e.target.value)
+      }}
+      py=".5rem"
+      as={Select}
+    >
+      <option value="studio">Studio apartment</option>
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+      <option value="4">4+</option>
+    </Input>
+  )
+}
+const RentDurationFilter = ({
+  handleSelection,
+}: {
+  handleSelection: ChangeEventHandler
+}) => {
+  const [value, setValue] = useState("")
+  return (
+    <Input
+      rounded=".5rem"
+      fontSize="1.6rem"
+      placeholder="Duration"
+      value={value}
+      onChange={(e) => {
+        handleSelection(e)
+        setValue(e.target.value)
+      }}
+      as={Select}
+      py=".5rem"
+    >
+      <option value="annually">Annually</option>
+      <option value="biannually">Biannually</option>
+      <option value="quarterly">Quarterly</option>
+      <option value="monthly">Monthly</option>
+    </Input>
+  )
+}
+const RentFilter = ({
+  handleSelection,
+}: {
+  handleSelection: ChangeEventHandler
+}) => {
+  const [value, setValue] = useState("")
+  return (
+    <Input
+      rounded=".5rem"
+      fontSize="1.6rem"
+      placeholder="Rent"
+      onChange={(e) => {
+        handleSelection(e)
+        setValue(e.target.value)
+      }}
+      value={value}
+      as={Select}
+      py=".5rem"
+    >
+      <option value="0-100000">
+        {"<= "}
+        {"₦"}
+        {(100000).toLocaleString("en-ng", {
+          maximumFractionDigits: 0,
+        })}
+      </option>
+      <option value="100001-200000">
+        {"₦"}
+        {(100001).toLocaleString("en-ng", {
+          maximumFractionDigits: 0,
+        })}{" "}
+        -{" ₦"}
+        {(200000).toLocaleString("en-ng", {
+          maximumFractionDigits: 0,
+        })}
+      </option>
+      <option value="200001-300000">
+        {"₦"}
+        {(200001).toLocaleString("en-ng", {
+          maximumFractionDigits: 0,
+        })}{" "}
+        -{" ₦"}
+        {(300000).toLocaleString("en-ng", {
+          maximumFractionDigits: 0,
+        })}
+      </option>
+      <option value="300001-400000">
+        {"₦"}
+        {(300001).toLocaleString("en-ng", {
+          maximumFractionDigits: 0,
+        })}{" "}
+        -{" ₦"}
+        {(400000).toLocaleString("en-ng", {
+          maximumFractionDigits: 0,
+        })}
+      </option>
+      <option value="400001-500000">
+        {"₦"}
+        {(400001).toLocaleString("en-ng", {
+          maximumFractionDigits: 0,
+        })}{" "}
+        -{" ₦"}
+        {(500000).toLocaleString("en-ng", {
+          maximumFractionDigits: 0,
+        })}
+      </option>
+      <option value="500000">
+        {"> ₦"}
+        {(500000).toLocaleString("en-ng", {
+          maximumFractionDigits: 0,
+        })}
+      </option>
+    </Input>
+  )
+}
--- app/ads/page.tsx
+++ app/ads/page.tsx
@@ -1,14 +1,16 @@
 "use client"
 import { Box, Heading } from "@chakra-ui/react"
-import { ReactNode, useEffect } from "react"
+import { ReactNode, useCallback, useEffect, useMemo, useState } from "react"
 import { useAppDispatch, useAppSelector } from "@/app/_redux"
 import { Listing } from "@/app/_types/Listings"
-import Loading from "../_assets/SVG/Loading"
-import Empty from "../_components/Empty"
 import ListingsGridLayout from "../_components/ListingsGridLayout"
 import RoomListingCard from "../_components/RoomListingCard"
 import { RoomListingCardSkeleton } from "../_components/Skeletons/ListingCardSkeleton"
 import { fetchListings } from "../_redux/thunks/search.thunk"
+import SearchBar from "../_components/Search/SearchBar"
+import useAxios from "../_hooks/useAxios"
+import NoResultsDisplay from "../_components/NoResultsDisplay"
+import { pluralizeText } from "../_utils"
 
 export default function Search() {
   const { hasFetchedInitialListings } = useAppSelector((store) => store.search)
@@ -18,30 +20,139 @@ export default function Search() {
   }, [dispatch, hasFetchedInitialListings])
   return <ListingsSection />
 }
-//search page
 function ListingsSection() {
+  const [page, setPage] = useState(1)
+  const [loadingSearch, setLoadingSearch] = useState(false)
+  const [cache, setCache] = useState({})
+  const [results, setResults] = useState([])
+  const [searchBarPositionAndZIndex, setSearchBarPositionAndZIndex] = useState({
+    top: "8rem",
+    zIndex: 1000,
+  })
   const { user } = useAppSelector((store) => store.auth)
   const { listings, loading } = useAppSelector((store) => store.search)
   if (listings.length === 0 && !loading) return null
+
+  useEffect(() => {
+    document.body.firstElementChild?.addEventListener("scroll", (e) => {
+      if (e.target) {
+        const container = e.target as any
+        if (container.scrollTop > 80) {
+          setSearchBarPositionAndZIndex({ top: "0", zIndex: 1000 })
+        } else {
+          setSearchBarPositionAndZIndex({ top: "8rem", zIndex: 100 })
+        }
+      }
+    })
+    return () => {
+      document.body.firstElementChild?.removeEventListener("scroll", () => {})
+    }
+  }, [])
+  const [searchCoordinates, setSearchCoordinates] = useState<null | {
+    lat: number
+    lng: number
+  }>(null)
+
+  const [rentAmount, setRentAmount] = useState<{
+    min?: number
+    max?: number
+  } | null>(null)
+  const [rentDuration, setRentDuration] = useState<
+    Listing["rentDuration"] | ""
+  >("")
+  const [bedrooms, setBedrooms] = useState<"studio" | number | "">("")
+
+  const searchQueryString = useMemo(() => {
+    let query = ""
+    if (searchCoordinates) {
+      query += `lat=${searchCoordinates.lat}&lng=${searchCoordinates.lng}&`
+    }
+    if (rentAmount) {
+      query += `${rentAmount.min ? `minRent=${rentAmount.min}&` : ""}${rentAmount.max ? `maxRent=${rentAmount.max}&` : ""}`
+    }
+    if (rentDuration) {
+      query += `rentDuration=${rentDuration}&`
+    }
+    if (bedrooms) {
+      query += `bedrooms=${bedrooms}`
+    }
+    return query
+  }, [rentAmount, rentDuration, bedrooms, searchCoordinates])
+  const { fetchData } = useAxios()
+  const search = useCallback(async () => {
+    if (loadingSearch) return
+    setLoadingSearch(true)
+    if ((cache as any)[searchQueryString]) {
+      setLoadingSearch(false)
+      return setResults([...((cache as any)[searchQueryString] || [])] as any)
+    }
+    const res = await fetchData({
+      url: `/listings/search?${searchQueryString}`,
+      method: "get",
+    })
+    setCache((prev) => ({ ...prev, [searchQueryString]: res.results }) as any)
+    setResults(res.results as any)
+    setLoadingSearch(false)
+  }, [searchQueryString, cache, loadingSearch])
+  useEffect(() => {
+    if (searchQueryString) search()
+  }, [search, searchQueryString, cache])
   return (
     <>
-      <>
-        <ListSectionContainer>
-          <Heading variant="md" fontWeight="500">Latest Rooms</Heading>
-          {loading ? (
-            <Box opacity=".8" mx="auto" w="100%" maxW="40rem">
-              <Loading />
-            </Box>
-          ) : (
-            <RoomsList
-              rooms={listings}
-              allowFavoriting={user !== null}
-              loading={loading}
-              emptyTextValue={<>No rooms found</>}
+      <Box maxW="125rem" mx="auto" pt={{ base: "13rem", md: "4rem" }}>
+        <Box
+          pos="fixed"
+          left="50%"
+          transform="translateX(-50%)"
+          w="100%"
+          style={searchBarPositionAndZIndex}
+          bg="white"
+        >
+          <Box
+            maxW="125rem"
+            px={{ base: "1.5rem", lg: "0rem" }}
+            shadow={{ base: "lg", md: "none" }}
+            py="1.8rem"
+            mx="auto"
+          >
+            <SearchBar
+              handleCoordinatesChange={(coordinates) =>
+                setSearchCoordinates(coordinates)
+              }
+              handleBedroomChange={(e) => {
+                setBedrooms(
+                  !isNaN(+e.target.value)
+                    ? +e.target.value
+                    : (e.target.value as any)
+                )
+              }}
+              handleRentChange={(e) => {
+                const minMax = e.target.value.split("-")
+                if (minMax.length > 1)
+                  setRentAmount({ min: +minMax[0], max: +minMax[1] })
+                else setRentAmount({ min: +minMax[0] })
+              }}
+              handleRentDurationChange={(e) =>
+                setRentDuration(e.target.value as any)
+              }
             />
-          )}
+          </Box>
+        </Box>
+        <ListSectionContainer>
+          <Heading variant="md" color="" fontWeight="500">
+            {searchQueryString
+              ? `${results.length} ${pluralizeText("listing", results.length, "s")} found`
+              : "Latest Rooms"}
+          </Heading>
+
+          <RoomsList
+            rooms={searchQueryString ? results : listings}
+            allowFavoriting={user !== null}
+            loading={loading || loadingSearch}
+            emptyTextValue={<>No rooms found. Try removing some filters</>}
+          />
         </ListSectionContainer>
-      </>
+      </Box>
     </>
   )
 }
@@ -80,13 +191,14 @@ function RoomsList({
   if (loading)
     return (
       <ListingsGridLayout
+        columns={{ base: 1, sm: 2, md: 2, lg: 4 }}
         list={new Array(12).fill(1).map((_, idx) => (
           <RoomListingCardSkeleton key={idx} />
         ))}
       />
     )
   if (rooms.length === 0 && !loading)
-    return <Empty heading="Oops" text={emptyTextValue} />
+    return <NoResultsDisplay heading="Oops" body={emptyTextValue} />
   return (
     <>
       <ListingsGridLayout
