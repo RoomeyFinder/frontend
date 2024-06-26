bec1733f7f46ea2ccc7cac8884a120bfac2e0de2
exploitenomah <iksploiting@gmail.com>
2024-06-26 14:36:54 +0100

    fix listing page error

--- app/ads/[listingId]/page.tsx
+++ app/ads/[listingId]/page.tsx
@@ -1,3 +1,4 @@
+import { capitalizeFirstLetter } from "@/app/_utils"
 import ClientContent from "./ClientContent"
 import { Listing } from "@/app/_types/Listings"
 
@@ -9,7 +10,7 @@ export async function generateMetadata({
   let res: Response | undefined
   try {
     res = await fetch(
-      `http://${process.env.SERVER_URL}/api/v1/listings/${params.listingId}`,
+      `${process.env.SERVER_URL}/api/v1/listings/${params.listingId}`,
       {
         method: "get",
       }
@@ -24,7 +25,7 @@ export async function generateMetadata({
       description: "The requested listing was not found.",
     }
   }
-  const title = `Stay with ${json?.listing?.owner?.firstName}`
+  const title = `Stay with ${capitalizeFirstLetter(json?.listing?.owner?.firstName || "")}`
   const description = `${json?.listing?.isStudioApartment ? "Studio apartment" : `${json?.listing?.numberOfBedrooms} bedroom apartment`} located at ${json?.listing?.streetAddress}`
   return {
     title,
