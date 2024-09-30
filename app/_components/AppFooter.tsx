e1095412f8bfe8e91de61f52089127450855868d
exploitenomah <iksploiting@gmail.com>
2024-09-30 13:15:46 +0100

    test fix for ads/slug metadata

--- app/_components/AppFooter.tsx
+++ app/_components/AppFooter.tsx
@@ -22,10 +22,10 @@ import { usePathname, useRouter } from "next/navigation"
 export default function AppFooter() {
   const pathname = usePathname()
 
-  console.log(pathname)
   if (
     pathname.startsWith("/ads") ||
-    pathname.startsWith("/users") || pathname.startsWith("/signup")
+    pathname.startsWith("/users") ||
+    pathname.startsWith("/signup")
   )
     return null
   return <AppFooterContent />
