7d783642035a8939e362233f5b3c91a3c4697cc1
exploitenomah <iksploiting@gmail.com>
2024-10-01 06:12:34 +0100

    update sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,48 +1,62 @@
 import { MetadataRoute } from "next"
 
+import citiesInNigeria from "./_data/citiesInNigeria.json"
+
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://roomeyfinder.netlify.app",
+      url: "https://roomeyfinder.com",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 1,
     },
     {
-      url: "https://roomeyfinder.netlify.app/about",
+      url: "https://roomeyfinder.com/about",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 0.8,
     },
     {
-      url: "https://roomeyfinder.netlify.app/contact",
+      url: "https://roomeyfinder.com/contact",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 0.8,
     },
     {
-      url: "https://roomeyfinder.netlify.app/login",
+      url: "https://roomeyfinder.com/login",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 0.8,
     },
     {
-      url: "https://roomeyfinder.netlify.app/signup",
+      url: "https://roomeyfinder.com/signup",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 0.8,
     },
     {
-      url: "https://roomeyfinder.netlify.app/terms-and-conditions",
+      url: "https://roomeyfinder.com/terms-and-conditions",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 0.8,
     },
     {
-      url: "https://roomeyfinder.netlify.app/privacy",
+      url: "https://roomeyfinder.com/privacy",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 0.8,
     },
+    {
+      url: "https://roomeyfinder.com/ads",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.8,
+    },
+    ...Object.keys(citiesInNigeria).map((city) => ({
+      url: `https://roomeyfinder.com/${city}`,
+      lastModified: new Date(Date.now()),
+      changeFrequency: "yearly" as any,
+      priority: 1,
+    })),
   ]
 }
