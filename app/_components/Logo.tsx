ec1d9ab638fef7071ac181491c57599991ffb7ba
exploitenomah <iksploiting@gmail.com>
2024-12-19 20:24:02 +0100

    properly size logo

--- app/_components/Logo.tsx
+++ app/_components/Logo.tsx
@@ -1,4 +1,5 @@
-import { Flex, Image, Text } from "@chakra-ui/react"
+import { Flex, Text } from "@chakra-ui/react"
+import Image from "next/image"
 import LogoText from "../_assets/SVG/LogoText"
 
 export default function AppLogo({
@@ -10,9 +11,10 @@ export default function AppLogo({
     <Flex gap="1rem" alignItems="center">
       <Image
         src="/images/logo.png"
-        width="3.588rem"
-        height="3rem"
+        width={35.8}
+        height={30}
         objectFit="contain"
+        style={{ width: "3.58rem", height: "auto" }}
         alt="RoomeyFinder Logo - Two magnifying glasses upside down connected by their handles forming an acute angle"
       />
       <Text
